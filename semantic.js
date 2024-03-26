76b9b4480eab3b31c94281c193072c14b294edb4
fcrlms <86426372+fcrlms@users.noreply.github.com>
2024-03-26 12:22:19 -0300

    Progress on AST handling

--- semantic.js
+++ semantic.js
@@ -1,4 +1,8 @@
-const { AssignAstNode, IfAstNode, UnaryAstNode, WhileAstNode, ForAstNode, ProgramAstNode } = require("./ast");
+const {
+    AssignAstNode, IfAstNode, UnaryAstNode, WhileAstNode,
+    ForAstNode, ProgramAstNode, VarDeclAstNode, CmdBlockAstNode,
+    ProcCallAstNode, BinaryAstNode, NumAstNode
+} = require("./ast");
 const { Token, Symbol } = require("./utils");
 
 class Scopes {
@@ -24,12 +28,13 @@ class Scopes {
     /**
      * Only checks the current scope
      * @param {String} key
+     * @returns {Boolean}
      */
     hasCurr(key) {
         // get closest scope on the stack
         if (this.stack.length) {
             const index = this.stack.length -1;
-            return this.stack[index];
+            return this.stack[index].has(key);
         }
 
         // else use the global scope
@@ -38,7 +43,7 @@ class Scopes {
 
     /**
      * @param {String} key
-     * @returns {*}
+     * @returns {ScopeEntry}
      */
     get (key) {
         for (let i = this.stack.length -1; i >= 0; i--) {
@@ -51,7 +56,7 @@ class Scopes {
 
     /**
      * @param {String} key
-     * @param {*} value
+     * @param {ScopeEntry} value
      */
     set(key, value) {
         if (this.stack.length) {
@@ -73,108 +78,259 @@ class Scopes {
     }
 }
 
+class ScopeEntry {
+    /**
+     * @param {Token} type
+     * @param {Array<>} args
+     */
+    constructor(type, args = undefined) {
+        this.type = type;
+        this.args = args;
+    }
+
+    /**
+     * returns true if `token` matches this type
+     * @param {Token} token
+     * @returns {Boolean}
+     */
+    isOfType(token) {
+        return this.type === token;
+    }
+
+    /**
+     * returns true if the symbols have the same type
+     * @param {ScopeEntry} entry1
+     * @param {ScopeEntry} entry2
+     * @returns {Boolean}
+     */
+    static haveSameType(entry1, entry2) {
+        return entry1.type === entry2.type;
+    }
+}
+
 const scope = new Scopes();
 
 /**
  * @param {ProgramAstNode} ast
  */
 module.exports = semantic = (ast) => {
-    scope.set(ast.id.lexeme, ast.id);
+    scope.set(ast.id.lexeme, new ScopeEntry(ast.id.type));
 
     if (ast.vardecl) {
-        const declarations = ast.vardecl.declarations
+        handleVariableDeclarations(ast.vardecl);
+    }
 
-        for (let declaration of declarations) {
-            const vartype = declaration.type;
-
-            for (let id of declaration.ids) {
-                if (scope.has(id.lexeme)) {
-                    console.log("Another variable already declared with this ID:", id.lexeme)
-                    continue;
-                }
-
-                scope.set(id.lexeme, vartype);
-            }
+    if (ast.subprogram) {
+        for (let procedure of ast.subprogram) {
+            handleProcedure(procedure);
         }
     }
 
-    //if (ast.subprogram) {
-    //  recursao
-    //}
-    // for qtdArgs scope.set(args)
-
-    if (ast.body) {
-
-        for (let command of ast.body.commands) {
-
-            if (command instanceof AssignAstNode) {
-
-                const vartype = scope.get(command.id.lexeme);
-
-                if (vartype) {
-
-                    if (command.symbol.type = Token.BOOLEAN) {
-                        //binary de relação ou unario NOT
-                        //caso ID ser TRUE ou FALSE
-                        //xingar se nao for algo acima
-                    }
-
-                    if (command.symbol.type = Token.VAR){
-                        //xingar se receber algo n var
-                    }
-
-                    if (command.symbol.type = Token.INT) {
-                        //verificar int e real se os tipos tao certos pela tabelinha
-                    }
-
-                    if (command.symbol.type = Token.REAL) {
-                        //mesmo do int
-                    }
-
-
-                } else {
-                    console.log (command.id.lexeme, " wasn't declared");
-                }
-                //handle expression
-            }
-
-            if (command instanceof IfAstNode) {
-
-                if (command.expr instanceof BinaryAstNode) {
-
-                    if (!isRelation(command.expr.symbol)) {
-                        console.log("not relational expression");
-                    }
-
-                } else {
-
-                    if (command.expr instanceof UnaryAstNode) {
-                        //NOT TODO
-                        if (!isRelation(command.expr.symbol)) {
-                            console.log("not relational expression");
-                        }
-
-                    } else {
-                        console.log("reclamar");//caso n precise do proc
-                    }
-                }
-            }
-
-            if (command instanceof WhileAstNode) {
-
-                //igual ao if? fazer funcao?
-
-            }
-
-            if (command instanceof ForAstNode) {
-
-                // num sei
-
-            }
-        }
-    }
-
+    handleCommandBlock(ast.body);
 };
+
+/**
+ * @param {CmdBlockAstNode} cmdblock
+ */
+function handleCommandBlock(cmdblock) {
+    for (let command of cmdblock.commands) {
+        handleCommand(command);
+    }
+}
+
+/**
+ * @param {(AssignAstNode | ProcCallAstNode | IfAstNode | WhileAstNode | ForAstNode)} command
+ */
+function handleCommand(command) {
+    if (command instanceof AssignAstNode) {
+        const vartype = scope.get(command.id.lexeme);
+
+        if (!vartype) {
+            console.log(`${command.id.lexeme} wasn't declared`);
+        }
+
+        //handle expression and warn on type mismatch
+    } else if (command instanceof IfAstNode) {
+        if (command.expr instanceof BinaryAstNode) {
+
+            if (!isRelation(command.expr.symbol)) {
+                console.log("not relational expression");
+            }
+
+        } else {
+
+            if (command.expr instanceof UnaryAstNode) {
+                //NOT TODO
+                if (!isRelation(command.expr.symbol)) {
+                    console.log("not relational expression");
+                }
+
+            } else {
+                console.log("reclamar");//caso n precise do proc
+            }
+        }
+    } else if (command instanceof WhileAstNode) {
+        //igual ao if? fazer funcao?
+    } else if (command instanceof ForAstNode) {
+        // num sei
+    } else if (command instanceof ProcCallAstNode) {
+        const procinfo = scopes.get(command.symbol.lexeme);
+
+        if (!procinfo) {
+            console.error(`${procinfo} is not declared.`);
+            return;
+        }
+
+        if (procinfo === Token.ID) { // is variable
+            console.error(`${procinfo} is not a procedure.`);
+            return;
+        }
+
+        // TODO: check if procedure is called correctly
+        // do after implementing procedures on scope
+        handleProcedureCall(command);
+    }
+}
+
+// TODO: create new enum for variable types, less headache
+
+/**
+ * @param {(BinaryAstNode | UnaryAstNode | ProcCallAstNode | NumAstNode)} expr
+ */
+function handleExpression (expr, expectedType = undefined) {
+    if (expr instanceof ProcCallAstNode) {
+        const entry = scope.get(expr.symbol.lexeme);
+        if (!entry) {
+            console.error(`Variable ${expr.symbol.lexeme} never declared.`);
+            return;
+        }
+
+        if (entry.isOfType(Token.PROCEDURE)) {
+            console.error(`Procedures do not have return value.`);
+            handleProcedureCall(expr);
+            return;
+        }
+
+        if (expr.args) {
+            console.error(`Variable ${expr.symbol.lexeme} is not a procedure.`);
+            return;
+        }
+
+        compareTypes(expectedType, entry);
+    }
+    else if (expr instanceof NumAstNode) {
+        compareTypes(expectedType, expr.symbol);
+    }
+    else if (expr instanceof UnaryAstNode) {
+        if (expr.symbol.type === Token.LPAREN) {
+            /**
+             * Since we reached a parenthesized expression, the parenthesis is
+             * no longer needed, we'll remove parentheses until we reach another
+             * expression and recurse, if an empty node is found report error
+             * because the expression has no value
+             */
+            while (expr && expr.symbol.type === Token.LPAREN) {
+                expr = expr.child;
+            }
+
+            if (expr) {
+                handleExpression(expr, expectedType);
+            } else {
+                console.error("Expression doesn't return value.");
+            }
+        }
+    }
+    else if (expr instanceof BinaryAstNode) {
+        // TODO
+    }
+}
+
+/**
+ * @param {Token} expectedType
+ * @param {(Symbol | ScopeEntry)} entry
+ */
+function compareTypes(expectedType, entry) {
+    if (entry.type === expectedType) {
+        return;
+    }
+
+    switch (expectedType) {
+    case Token.BOOLEAN: {
+        console.error(`Expected boolean, got ${entry.type}`)
+        break;
+    }
+    case Token.INT: {
+        console.error(`Expected integer, got ${entry.type}`)
+        break;
+    }
+    case Token.REAL: {
+        console.error(`Expected real, got ${entry.type}`)
+        break;
+    }
+    default: {}
+    }
+}
+
+/**
+ * @param {ProcCallAstNode} procCall
+ */
+function handleProcedureCall(procCall) {
+    const entry = scope.get(expr.symbol.lexeme);
+    const expectedArgSize = entry.args.length;
+    const actualArgSize = procCall.args.length;
+
+    if (actualArgSize < expectedArgSize) {
+        console.error(`Too little arguments received, expected ${expectedArgSize}`);
+    } else if (actualArgSize > expectedArgSize) {
+        console.error(`Too much arguments received, expected ${actualArgSize}`);
+    }
+
+    const min = min(expectedArgSize, actualArgSize);
+
+    // handle actual args here until we reach the desired amount
+    // or until the args provided end
+    for (let i = 0; i < min; ++i) {
+        const expr = procCall.args[i];
+        handleExpression(expr, entry.args[i]);
+    }
+
+    // handle extra arguments here but expect no value
+    for (let i = min; i < actualArgSize; ++i) {
+        const expr = procCall.args[i];
+        handleExpression(expr);
+    }
+}
+
+/**
+ * @param {VarDeclAstNode} vardecl
+ */
+function handleVariableDeclarations(vardecl) {
+    const declarations = vardecl.declarations
+
+    for (let declaration of declarations) {
+        const vartype = declaration.type;
+
+        for (let id of declaration.ids) {
+            if (scope.hasCurr(id.lexeme)) {
+                console.error("Another variable already declared with this ID:", id.lexeme);
+                continue;
+            }
+
+            scope.set(id.lexeme, new ScopeEntry(vartype));
+        }
+    }
+}
+
+/**
+ * @param {ProgramAstNode} procedure
+ */
+function handleProcedure(procedure) {
+    scope.addNewScope();
+
+    // handle here
+
+    scope.removeScope();
+}
 
 /**
  * @param {Symbol} symbol
